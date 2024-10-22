7e3a5346409e4424de2d6bef1970ca31211b970b
Bappi1234 <harunbappi1234@gmail.com>
2024-10-22 20:39:47 +0600

    visitd button added with dynamic style

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import '../country.css'
 export default function Country({country}) {
-   const {name,flags} = country
+   const {name,flags, population,area,cca3} = country
+   const [visited, setVisited] = useState(false)
+
+   const handleVisited = ()=>{
+    setVisited(!visited)
+   }
   return (
     <div className="box">
         <h3>Name: {name.common}</h3>
         <img src={flags.png} alt="" />
+        <p>Population: {population}</p>
+        <p>Area: {area}</p>
+        <p>Code: {cca3}</p>
+        <button onClick={handleVisited}>{visited?"Done":'Going'}</button>
+        {visited ?'I visited this Country':'I Going to Visit'}
     </div>
   )
 }
